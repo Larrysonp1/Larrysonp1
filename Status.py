04cec57b79da90b82cfcff469eef92bf028b12df
Larrysonp1 <66893179+Larrysonp1@users.noreply.github.com>
2022-12-10 02:15:51 +0000

    Delete Status.py

--- Status.py
+++
@@ -1,8 +0,0 @@
-Teams = {"Barca":"Active", "Chelsea":"inactive", "Manchester":"inactive", "Madrid":"Active"}
-
-for team,status in Teams.copy().items():
-    if status == "Active":
-        print(team, status , sep=" - ")
-
-  #  else:
-  #     print("This teams are inactive.") 
